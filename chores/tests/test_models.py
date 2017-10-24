78aba9e8d0af5d658a672f3580d9f3d65d8a1bb6
Connor Hanley <connorhanley@ymail.com>
2017-10-23 22:50:03 -0400

    Added factories and contexts for Children, Tasks, and Chores

---
+++ chores/tests/test_models.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+from books.models import *
+from books.tests.utilities import *
+
