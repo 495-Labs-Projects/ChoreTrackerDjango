d84c07b3f41d8a5d44ea6706e7b72248fd43ce09
Richard Huang <rahuang@andrew.cmu.edu>
2017-11-04 17:37:40 -0400

    Created the start code without any of the test

--- chores/tests/test_functionals/__init__.py
+++
@@ -1,4 +0,0 @@
-from .test_base_functionals import *
-from .test_child_functionals import *
-from .test_task_functionals import *
-from .test_chore_functionals import *
--- chores/tests/test_views/__init__.py
+++
@@ -1,3 +0,0 @@
-from .test_child_views import *
-from .test_task_views import *
-from .test_chore_views import *
